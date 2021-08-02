a1e6820d7a1c8bbf91cd45d24dee7fa2a0415fb9
Jordan Hanley <Jordan.Hanley@bennetts.co.uk>
2021-08-02 10:09:44 +0100

    add redux store for worker hand state

--- src/states/worker-hand.ts
+++ src/states/worker-hand.ts
@@ -1,13 +1,35 @@
 import { Action } from "./index";
 
-export interface WorkerHandState {}
+const ACTION_LOWER_HAND = 'LOWER_HAND';
+const ACTION_RAISE_HAND = 'RAISE_HAND';
 
-const initialState: WorkerHandState = {};
+export interface WorkerHandState {
+  isRaised: boolean,
+}
 
-export class Actions {}
+const initialState: WorkerHandState = {
+  isRaised: false,
+};
+
+export class Actions {
+  public static lowerHand = (): Action => ({ type: ACTION_LOWER_HAND });
+  public static rasieHand = (): Action => ({ type: ACTION_RAISE_HAND });
+}
 
 export function reduce(state: WorkerHandState = initialState, action: Action) {
   switch (action.type) {
+    case ACTION_LOWER_HAND: {
+      return {
+        ...state,
+        isRaised: false,
+      };
+    }
+    case ACTION_RAISE_HAND: {
+      return {
+        ...state,
+        isRaised: true,
+      };
+    }
     default:
       return state;
   }
